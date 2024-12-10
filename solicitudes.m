710724b0a56318c8e5cd9f4fbbe268be97cf8412
KevAraujo <kevin.araujo@cinvestav.mx>
2024-12-10 16:47:30 -0600

    Create solicitudes.m

---
+++ solicitudes.m
@@ -0,0 +1,8 @@
+function [num_puertos,num_impedancias,frec1,frec2,paso] = Solicitudes
+
+num_puertos=input('numero de puertos: ');
+num_impedancias=input('numero de impedancias = ');
+frec1=input('frecuencia inicial: ');
+frec2=input('frecuencia final: ');
+paso=input('tamaño de paso ');
+end
